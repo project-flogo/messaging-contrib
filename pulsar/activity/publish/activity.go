8af5707ea83c4a9a52f6b7efef98edc001bd528c
Wendell Nichols <wnichols@tibco.com>
2020-09-21 10:24:24 -0600

    Runtime changes related to the Enterprise Design

--- pulsar/activity/publish/activity.go
+++ pulsar/activity/publish/activity.go
@@ -6,15 +6,19 @@ import (
 
 	"github.com/apache/pulsar-client-go/pulsar"
 	"github.com/project-flogo/core/activity"
+	"github.com/project-flogo/core/data"
 	"github.com/project-flogo/core/data/coerce"
 	"github.com/project-flogo/core/data/metadata"
+	"github.com/project-flogo/core/support/log"
 )
+
+var logger = log.ChildLogger(log.RootLogger(), "pulsar-publish")
 
 func init() {
 	_ = activity.Register(&Activity{}, New)
 }
 
-var activityMd = activity.ToMetadata(&Settings{}, &Input{})
+var activityMd = activity.ToMetadata(&Settings{}, &Input{}, &Output{})
 
 //New optional factory method, should be used if one activity instance per configuration is desired
 func New(ctx activity.InitContext) (activity.Activity, error) {
@@ -32,15 +36,28 @@ func New(ctx activity.InitContext) (activity.Activity, error) {
 	}
 
 	client = pulsarConn.GetConnection().(pulsar.Client)
-
-	producer, err := client.CreateProducer(pulsar.ProducerOptions{
-		Topic: s.Topic,
-	})
-
+	if ctx.Settings()["topic"] == nil {
+		return nil, fmt.Errorf("New: no topic specified")
+	}
+	producerOptions := pulsar.ProducerOptions{
+		Topic: ctx.Settings()["topic"].(string),
+	}
+	if ctx.Settings()["compressionType"] != nil {
+		switch ctx.Settings()["compressionType"].(string) {
+		case ("LZ4"):
+			producerOptions.CompressionType = pulsar.LZ4
+		case ("ZLIB"):
+			producerOptions.CompressionType = pulsar.ZLib
+		case ("ZSTD"):
+			producerOptions.CompressionType = pulsar.ZSTD
+		default:
+			producerOptions.CompressionType = pulsar.NoCompression
+		}
+	}
+	producer, err := client.CreateProducer(producerOptions)
 	if err != nil {
 		return nil, fmt.Errorf("Could not instantiate Pulsar producer: %v", err)
 	}
-
 	act := &Activity{producer: producer}
 
 	return act, nil
@@ -64,15 +81,39 @@ func (a *Activity) Eval(ctx activity.Context) (done bool, err error) {
 	if err != nil {
 		return true, err
 	}
-	defer a.producer.Close()
-	msg := &pulsar.ProducerMessage{
-		Payload: []byte(input.Payload),
+	var msgBytes interface{}
+	if input.Payload != nil {
+		msgBytes, err = coerce.ToType(input.Payload, data.TypeBytes)
+		if err != nil {
+			return true, err
+		}
+	} else {
+		return false, fmt.Errorf("Publisher: no message payload provided")
+	}
+	msg := pulsar.ProducerMessage{
+		Payload: msgBytes.([]byte),
+	}
+	if input.Properties != nil {
+		props, err := coerce.ToType(input.Properties, data.TypeParams)
+		if err != nil {
+			return true, err
+		}
+		logger.Debugf("Publisher payload properties: %v", input.Properties)
+		msg.Properties = props.(map[string]string)
+	}
+	if input.Key != "" {
+		logger.Debugf("Publisher payload key: %s", input.Key)
+		keyStr, err := coerce.ToType(input.Key, data.TypeString)
+		if err != nil {
+			return true, err
+		}
+		msg.Key = keyStr.(string)
 	}
 
-	_,err = a.producer.Send(context.Background(), msg)
+	msgID, err := a.producer.Send(context.Background(), &msg)
 	if err != nil {
-		return true, fmt.Errorf("Producer could not send message: %v", err)
+		return true, fmt.Errorf("Publisher could not send message: %v", err)
 	}
-
+	ctx.SetOutput("msgid", fmt.Sprintf("%x", msgID.Serialize()))
 	return true, nil
 }
--- pulsar/connection/README.md
+++ pulsar/connection/README.md
@@ -10,9 +10,13 @@ flogo install github.com/project-flogo/messaging-contrib/pulsar/connection
 ## Configuration
 
 ### Settings: 
-| Name       | Type   | Description
-|:---        | :---   | :---   
-| url        | string | The url used to connect to pulsar - ***REQUIRED***
-| athenzAuth | params | The params used for Athenz Authentication
-| certFile   | string | The location of the certificate file used in TLS.
-| keyFile    | string | The location of the key file used in TLS.
+| Name          | Type   | Description
+|:---           | :---   | :---   
+| url           | string | The url used to connect to pulsar - ***REQUIRED***
+| auth          | string | The type of authentication used: None, TLS, JWT, Athenz
+| allowInsecure | bool   | Allow self signed certs or not
+| athenzAuth    | params | The params used for Athenz Authentication
+| jwt           | string | The JWT authentication token
+| caCert        | string | The location of the ca cert file used in TLS.
+| certFile      | string | The location of the certificate file used in TLS.
+| keyFile       | string | The location of the key file used in TLS.
